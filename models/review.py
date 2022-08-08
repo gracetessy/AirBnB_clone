9a6a184167cf499b516e1b155109d7f29413d108
gracetessy <graceabrahamimaji@gmail.com>
2022-08-09 00:31:09 +0100

    review.py

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines review class
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Reviews made by users about a place"""
+    place_id = ""
+    user_id = ""
